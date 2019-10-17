955510b02d1d61030e4ff97be69a337a04b9854a
b117014 <b117014@iiit-bh.ac.in>
2019-10-17 18:32:27 +0530

    update arrayList

--- Array_List.java
+++ Array_List.java
@@ -4,7 +4,7 @@ import java.util.*;
 class Array_List{
 
 	public static void main(String args[]){
-		 ArrayList<Integer> arr = new ArrayList<Integer>();
+		 ArrayList<Integer> arr = new ArrayList<Integer>();     //type of ArrayList are Integer,String,Boolean,Character
 		 arr.add(5);
 		 arr.add(6);
 		 arr.add(1);
@@ -12,7 +12,8 @@ class Array_List{
 		arr.get(0);            //  get element at ith index
 		arr.remove(0);         // remove element at 0th index
 		arr.set(1,9);         // modify elemnt at 1th index with value 9
-		 System.out.println(arr);
+		arr.size()              // it will return size of array list
+		 System.out.println(arr);  // print all element present in array
 
 		// String arr1 = "hello";
 		// String arr2 = "hello";
